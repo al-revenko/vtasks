ee9bbd034995bcf3823f67a3e76884a2e422b22c
Alexander Revenko <revenko.a@bk.ru>
2024-03-20 17:07:19 +0700

    Сделал компонент PageLayout

--- src/components/PageLayout.vue
+++ src/components/PageLayout.vue
@@ -1,13 +1,17 @@
 <template>
-  <div :class="'w-full min-h-screen bg-slate-600'">
-    <div :class="'max-w-screen-2xl h-full bg-slate-400 mx-auto box-border'">
-      <slot></slot>
+  <div :data-id="'PageLayout'" :class="'w-full bg-orange-500'">
+    <div
+      :data-id="'PageLayout__grid-conatiner'"
+      :class="'max-w-screen-2xl min-h-screen mx-auto bg-white grid lg:grid-cols-[90px_1fr_90px] lg:grid-rows-1 grid-rows-[50px_1fr]'"
+    >
+      <slot :name="'bar'" />
+      <div :data-id="'PageLayout__content'" :class="'px-3 pt-5'">
+        <slot />
+      </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts"></script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
