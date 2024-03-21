d3e7bc067b50f069f85bc7c7b65bcf8f2cde5ded
Alexander Revenko <revenko.a@bk.ru>
2024-03-21 14:53:34 +0700

    Сделал компонент CheckboxInput Добавил пользовательский класс fade

---
+++ src/components/ui/CheckboxInput.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <input
+      v-model="checkedModel"
+      :type="'checkbox'"
+      :class="'checkbox border-slate-400 checked:border-lime-500 [--chkfg:white] [--chkbg:theme(colors.lime.500)] ' + size"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineModel } from 'vue'
+
+type SizeType = 'xs' | 'sm'| 'md'| 'lg'
+
+const props = defineProps<{
+  size: SizeType  
+}>()
+
+const checkedModel = defineModel<boolean>()
+
+const size = 'checkbox-' + props.size
+</script>
