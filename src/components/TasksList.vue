d83549e177a12c4b9edafb8229cdbdc2b28cb1c8
Alexander Revenko <revenko.a@bk.ru>
2024-03-21 20:40:42 +0700

    Сделал компонент TasksList

---
+++ src/components/TasksList.vue
@@ -0,0 +1,19 @@
+<template>
+  <ul :class="'flex gap-5 flex-col'">
+    <li v-for="task in tasks" :key="task.createdAt">
+      <div :class="'z-10 flex gap-2'">
+        <div :class="'pt-0.5'">
+          <CheckboxInput v-model="task.isDone" :size="'xs'" />
+        </div>
+        <span>{{ task.title }}</span>
+      </div>
+    </li>
+  </ul>
+</template>
+
+<script setup lang="ts">
+import CheckboxInput from './ui/CheckboxInput.vue'
+import type { ITask } from '@/interfaces/task.interface'
+
+const tasks = defineModel<ITask[]>({ required: true })
+</script>
