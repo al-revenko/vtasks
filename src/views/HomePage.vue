b202de4e55c2996351a53dfdd3c1b0b8b3914dbe
Alexander Revenko <revenko.a@bk.ru>
2024-03-27 23:11:58 +0700

    Сделал view - HomePage

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,11 +1,47 @@
 <template>
   <PageLayout>
-    <div :class="'bg-orange-400 w-64 h-60'"></div>
+    <template #bar>
+      <MenuBar />
+    </template>
+    <CardsLayout>
+      <template v-for="task in tasks" :key="task.id">
+        <TaskCard
+          v-if="isMacroTask(task)"
+          :id="task.id"
+          :title="task.title"
+          :desc="task.desc"
+          @m-tasks-update="collbacks.onMacroTaskUpdate"
+          v-model:is-done="task.isDone"
+          v-model:nested-data="task.nestedData"
+        />
+        <TaskCard
+          v-else
+          :id="task.id"
+          :title="task.title"
+          :desc="task.desc"
+          v-model:is-done="task.isDone"
+        />
+      </template>
+    </CardsLayout>
   </PageLayout>
 </template>
 
 <script setup lang="ts">
 import PageLayout from '@/components/PageLayout.vue'
-</script>
+import MenuBar from '@/components/MenuBar.vue'
+import CardsLayout from '@/components/CardsLayout.vue'
+import TaskCard from '@/components/TaskCard.vue'
+import useTaskStore from '@/stores/task.store'
+import isMacroTask from '@/guards/isMacroTask.guard'
 
-<style scoped></style>
+const taskStore = useTaskStore()
+
+const collbacks = {
+  onMacroTaskUpdate(id: number) {
+    taskStore.updateMacroTask(id)
+  } 
+}
+
+const tasks = taskStore.$state.tasks
+
+</script>
