ed584845cb2b37f39520eb9dc370a1bcf011fd32
Alexander Revenko <revenko.a@bk.ru>
2024-03-27 23:11:40 +0700

    Сделал компонент CardsLayout

---
+++ src/components/CardsLayout.vue
@@ -0,0 +1,10 @@
+<template>
+  <div :data-id="'CardsLayout'" :class="'flex justify-center'">
+    <div
+      :data-id="'CardsLayout__grid'"
+      :class="'grid gap-10 grid-cols-[max-content_max-content_max-content_max-content]'"
+    >
+      <slot></slot>
+    </div>
+  </div>
+</template>
