85adab0a1537913bbc7f5d8b76819af11e821ed1
Alexander Revenko <revenko.a@bk.ru>
2024-03-29 10:45:19 +0700

    Сделал компонент ModalWindow

---
+++ src/components/ui/ModalWindow.vue
@@ -0,0 +1,31 @@
+<template>
+  <div
+    data-id="ModalWindow"
+    :class="'flex items-center justify-center h-screen w-screen fixed left-0 top-0 backdrop-blur-sm'"
+    v-show="isShowModel ? true : ''"
+  >
+    <div :class="'min-h-80 min-w-80 bg-white border rounded-md drop-shadow-lg relative p-4'">
+      <div :class="'flex justify-between  gap-2 items-center'">
+        <h2 :class="'text-slate-600'">{{ props.title }}</h2>
+        <CloseBtn :class="'btn-sm'" @click="() => callbacks.onClose()" />
+      </div>
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import CloseBtn from '@/components/ui/CloseBtn.vue'
+
+const props = defineProps<{
+  title?: string
+}>()
+
+const isShowModel = defineModel<boolean>({ required: true })
+
+const callbacks = {
+  onClose() {
+    isShowModel.value = false
+  },
+}
+</script>
