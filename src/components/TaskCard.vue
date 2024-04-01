0d92ec91e56e6c00debb303f047109305f7f12ab
Alexander Revenko <revenko.a@bk.ru>
2024-04-01 23:59:43 +0700

    Добавил шаблон если нет описания задачи в TaskCard

--- src/components/TaskCard.vue
+++ src/components/TaskCard.vue
@@ -1,41 +1,60 @@
 <template>
-  <button
-    data-id="TaskCard"
-    :href="'#'"
-    :class="
-      `grid grid-rows-[max-content_1fr] gap-3 h-72 w-64 pt-3 pl-3 pr-2 pb-3 text-start transition-all ease-in duration-200 bg-white border rounded-md shadow-[-1px_2px_6px_1px]` +
-      ' ' +
-      shadowColor
-    "
-    @click="emit('click', props.id)"
-  >
-    <div :class="'flex items-start justify-between gap-3'">
-      <h3 :class="'text-lg font-medium'">{{ props.title }}</h3>
-      <div :class="'pt-1'">
-        <CheckboxInput @click.stop v-if="!hasTasks" v-model="isDoneModel" :size="'lg'" />
+  <template v-if="props.desc">
+    <button
+      data-id="TaskCard"
+      :class="
+        `grid grid-rows-[max-content_1fr] gap-3 h-72 w-64 pt-3 pl-3 pr-2 pb-3 text-start transition-all ease-in duration-200 bg-white border rounded-md shadow-[-1px_2px_6px_1px]` +
+        ' ' +
+        shadowColor
+      "
+      @click="emit('click', props.id)"
+    >
+      <div :class="'flex items-start justify-between gap-3'">
+        <h3 :class="'text-lg font-medium'">{{ props.title }}</h3>
+        <div :class="'pt-1'">
+          <CheckboxInput @click.stop v-if="!hasTasks" v-model="isDoneModel" :size="'lg'" />
+        </div>
       </div>
-    </div>
-
-    <template v-if="macroTaskData && hasTasks">
-      <div :class="'pl-1 pr-5'">
-        <TasksList
-          :class="'flex gap-1 flex-col flex-wrap h-44 overflow-hidden'"
-          v-model="macroTaskData.tasks"
-        />
+  
+      <template v-if="macroTaskData && hasTasks">
+        <div :class="'pl-1 pr-5'">
+          <TasksList
+            :class="'flex gap-1 flex-col flex-wrap h-44 overflow-hidden'"
+            v-model="macroTaskData.tasks"
+          />
+        </div>
+        <div :class="'h-6 pb-2'">
+          <ProgressBar :percentage="donePercentage" :title="donePercentage + '%'" />
+        </div>
+      </template>
+  
+      <template v-else>
+        <div :class="'fade overflow-hidden h-full'">
+          <p v-if="props.desc">
+            {{ props.desc }}
+          </p>
+        </div>
+      </template>
+    </button>
+  </template>
+  <template v-else>
+    <a
+      data-id="TaskCard"
+      :href="'/'"
+      :class="
+        `flex justify-center items-center h-72 w-64 pt-3 pl-3 pr-2 pb-3 transition-all ease-in duration-200 bg-white border rounded-md shadow-[-1px_2px_6px_1px]` +
+        ' ' +
+        shadowColor
+      "
+    >
+      <div :class="'flex items-start justify-between gap-3'">
+        <h3 :class="'text-lg font-medium'">{{ props.title }}</h3>
+        <div :class="'pt-1'">
+          <CheckboxInput @click.stop v-if="!hasTasks" v-model="isDoneModel" :size="'lg'" />
+        </div>
       </div>
-      <div :class="'h-6 pb-2'">
-        <ProgressBar :percentage="donePercentage" :title="donePercentage + '%'" />
-      </div>
-    </template>
-
-    <template v-else>
-      <div :class="'fade overflow-hidden h-full'">
-        <p v-if="props.desc">
-          {{ props.desc }}
-        </p>
-      </div>
-    </template>
-  </button>
+    </a>
+  </template>
 </template>
 
 <script setup lang="ts">
