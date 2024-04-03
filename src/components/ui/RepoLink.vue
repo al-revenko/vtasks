2454dd2ea3f4bc4dc60f3697b1c8d85308c4f250
Alexander Revenko <revenko.a@bk.ru>
2024-04-03 16:18:16 +0700

    Сделал новый ui компонент RepoLink

---
+++ src/components/ui/RepoLink.vue
@@ -0,0 +1,12 @@
+<template>
+  <a href="https://github.com/al-revenko/vtasks" target="_blank" rel="noopener noreferrer">
+    <GitHubSVG
+      :class="'h-14 w-14 transition-colors ease-in duration-100 fill-white hover:fill-slate-100 active:fill-slate-300'"
+    />
+  </a>
+</template>
+
+<script setup lang="ts">
+import GitHubSVG from '@/components/svg/GitHubSVG.vue'
+</script>
+
