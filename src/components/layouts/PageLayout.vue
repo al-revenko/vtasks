4e8edec372f758bb7115d3d2ff56d670cf68a837
Alexander Revenko <revenko.a@bk.ru>
2024-04-03 16:17:03 +0700

    Установил и дополнил тему daisyUI Небольшие изменения в стилях PageLayout

--- src/components/layouts/PageLayout.vue
+++ src/components/layouts/PageLayout.vue
@@ -1,8 +1,8 @@
 <template>
-  <div :data-id="'PageLayout'" :class="'w-full bg-orange-500'">
+  <div :data-id="'PageLayout'" :class="'w-full bg-slate-50'">
     <div
       :data-id="'PageLayout__grid-conatiner'"
-      :class="'max-w-screen-2xl min-h-screen mx-auto bg-white grid lg:grid-cols-[90px_1fr_90px] lg:grid-rows-1 grid-rows-[50px_1fr]'"
+      :class="'max-w-screen-2xl min-h-screen mx-auto grid lg:grid-cols-[120px_1fr_120px] lg:grid-rows-1 grid-rows-[50px_1fr]'"
     >
       <slot :name="'bar'" />
       <div :data-id="'PageLayout__content'" :class="'px-3 pt-5'">
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './public/**/*.html',
-    './src/**/*.{js,jsx,ts,tsx,vue}',
-  ],
+  darkMode: 'false',
+  content: ['./public/**/*.html', './src/**/*.{js,jsx,ts,tsx,vue}'],
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui")],
+    daisyui: {
+    themes: [
+      {
+        nord: {
+          ...require("daisyui/src/theming/themes")["nord"],
+          "success": "#87d039",
+          "error": "#e11d48"
+        },
+      },
+    ],
+  },
+  plugins: [require('daisyui')],
 }
