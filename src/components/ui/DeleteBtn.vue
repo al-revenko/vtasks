17582cc6420b229a218c7fd30eee67b5cf12c261
Alexander Revenko <revenko.a@bk.ru>
2024-04-16 14:39:17 +0700

    Новые компоненты: BackPageBtn DeleteBtn

---
+++ src/components/ui/DeleteBtn.vue
@@ -0,0 +1,38 @@
+<template>
+  <button
+    data-id="DeleteBtn"
+    :type="'button'"
+    :class="`btn btn-square btn-outline btn-ghost border-none`"
+  >
+    <TrashCanSVG :class="colorClasses.svg"/>
+  </button>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue';
+import TrashCanSVG from '@/components/svg/TrashCanSVG.vue'
+
+const props = defineProps<{
+  color?: 'neutral' | 'white'
+}>()
+
+const colorClasses = computed<{
+  btn: string
+  svg: string
+}>(() => {
+  const colors = { svg: '', btn: '' }
+
+  switch (props.color) {
+    case 'white':
+      colors.svg = `fill-white hover:fill-slate-100`
+      return colors
+
+    default:
+      colors.svg = `fill-neutral hover:fill-slate-600`
+      return colors
+  }
+})
+</script>
+
+<style scoped></style>
+
