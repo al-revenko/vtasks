2050b1c415e63d67ad724685f677846f32b85a91
Alexander Revenko <revenko.a@bk.ru>
2024-04-22 15:21:28 +0700

    Новые компоненты: DoneBtn CheckMarkSVG

---
+++ src/components/ui/DoneBtn.vue
@@ -0,0 +1,13 @@
+<template>
+  <BtnLayout :data-id="'DoneBtn'" :color="props.color ?? 'neutral'">
+    <CheckMarkSVG :class="`w-full h-full`" />
+  </BtnLayout>
+</template>
+
+<script setup lang="ts">
+import BtnLayout from '@/components/layouts/BtnLayout.vue'
+import CheckMarkSVG from '@/components/svg/CheckMarkSVG.vue';
+const props = defineProps<{
+  color?: 'neutral' | 'white'
+}>()
+</script>
