37ff7433b775a4b95af300272c5ebe37468afade
Alexander Revenko <revenko.a@bk.ru>
2024-04-30 14:01:20 +0700

    Завершил работу над TaskPage Новый компонент: TaskContent

---
+++ src/components/contents/TaskContent.vue
@@ -0,0 +1,88 @@
+<template>
+  <div data-id="TaskContent" :class="`lg:pl-6 pb-8`">
+    <header :class="`lg:pt-6`">
+      <EditableTitle
+        :class="`mx-auto sm:mx-0 sm:max-w-lg max-w-72`"
+        placeholder="Имя задачи"
+        :max-length="titleMaxLength"
+        :required="true"
+        v-model="taskModel.title"
+      >
+        <TitleMain :class="`max-w-full`">{{ taskModel.title }}</TitleMain>
+      </EditableTitle>
+    </header>
+    <main :class="`w-full overflow-x-hidden`">
+      <section :class="`pt-6 text-center sm:text-left`">
+        <TitleSecond>Время создания</TitleSecond>
+        <p :class="`pt-6 lg:pl-4`">{{ createdAt }}</p>
+      </section>
+      <section :class="`pt-8`">
+        <EditableTaskList
+          :max-tasks="microTasksMaxCount"
+          :placeholder="`Имя подзадачи`"
+          :sufix="taskCountSuffix"
+          v-model="microTasks"
+          @add="callbacks.onMicroTaskAdd"
+        >
+          <TitleSecond>Подзадачи</TitleSecond>
+        </EditableTaskList>
+      </section>
+      <section :class="`pt-8`">
+        <EditableText v-model="taskModel.desc" :max-length="descMaxLength">
+          <TitleSecond>Описание</TitleSecond>
+        </EditableText>
+      </section>
+    </main>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+import { microTasksMaxCount, titleMaxLength, descMaxLength } from '@/inputConfig'
+import type { ITask } from '@/types/task.interface'
+import useTimeFormatter from '@/composables/useTimeFormatter'
+import usePlural from '@/composables/usePlural'
+import TitleMain from '@/components/ui/TitleMain.vue'
+import TitleSecond from '@/components/ui/TitleSecond.vue'
+import EditableTitle from '@/components/EditableTitle.vue'
+import EditableTaskList from '@/components/EditableTaskList.vue'
+import EditableText from '@/components/EditableText.vue'
+
+const taskModel = defineModel<ITask>({ required: true })
+const emit = defineEmits(['microtaskAdd'])
+
+const microTasks = computed(() => {
+  if (taskModel.value.nested) {
+    return taskModel.value.nested.tasks
+  }
+
+  return []
+})
+
+const time = useTimeFormatter(taskModel.value.createdAt, 'D MMMM YYYY HH:mm').value.split(' ')
+
+const createdAt = `${time[0]} ${time[1]} ${time[2]} года, в ${time[3]}`
+
+const taskCountSuffix = computed(() => {
+  if (taskModel.value.nested === null) {
+    return ''
+  }
+
+  const suffixes = {
+    zero: 'задач',
+    one: 'задача',
+    two: 'задачи',
+    few: 'задачи',
+    many: 'задач',
+    other: 'задач',
+  }
+  const rule = usePlural(taskModel.value.nested.tasks.length).value
+  return suffixes[rule]
+})
+
+const callbacks = {
+  onMicroTaskAdd(title: string) {
+    emit('microtaskAdd', title)
+  },
+}
+</script>
--- src/views/TaskPage.vue
+++ src/views/TaskPage.vue
@@ -1,13 +1,33 @@
 <template>
-  <div :class="`bg-blue-500 w-full h-10`">
-    {{ task?.title }}
-  </div>
+  <PageLayout>
+    <template #bar>
+      <TaskBar
+        v-if="task.nested"
+        :percentage="percentage"
+        v-model:status="task.isDone"
+        @status-change="callbacks.onStatusChange"
+        @delete="callbacks.onDelete"
+      />
+      <TaskBar
+        v-else
+        v-model:status="task.isDone"
+        @status-change="callbacks.onStatusChange"
+        @delete="callbacks.onDelete"
+      />
+    </template>
+    <TaskContent v-model="task" @microtask-add="callbacks.onMicroTaskAdd" />
+  </PageLayout>
 </template>
 
 <script setup lang="ts">
+import { computed } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
 import { useTaskStore } from '@/stores/task.store'
-import type { IMacroTask, ITask } from '@/types/task.interface'
+import { type ITask } from '@/types/task.interface'
+import usePercentage from '@/composables/usePercentage'
+import PageLayout from '@/components/layouts/PageLayout.vue'
+import TaskBar from '@/components/TaskBar.vue'
+import TaskContent from '@/components/contents/TaskContent.vue'
 
 const taskStore = useTaskStore()
 const route = useRoute()
@@ -19,5 +39,22 @@ if (!result) {
   router.push({ name: 'home' })
 }
 
-const task = result as ITask | IMacroTask
+const task = result as ITask
+
+const percentage = computed(() => usePercentage(task.nested).value)
+
+const callbacks = {
+  onStatusChange() {
+    taskStore.forceTaskStatus(task.id, task.isDone)
+  },
+
+  onDelete() {
+    taskStore.deleteTaskByID(task.id)
+    router.push({ name: 'home' })
+  },
+
+  onMicroTaskAdd(title: string) {
+    taskStore.addMicroTasks(task, [title])
+  },
+}
 </script>
